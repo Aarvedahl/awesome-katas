b5e99b973e364b6aa7178164cd5b7cbc86ab09c3
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-06-11 19:43:33 +0200

    Begin with memo

--- src/main/java/io/github/aarvedahl/Knapsack/Knapsack.java
+++ src/main/java/io/github/aarvedahl/Knapsack/Knapsack.java
@@ -1,26 +1,32 @@
 package io.github.aarvedahl.Knapsack;
 
 
+import java.util.stream.IntStream;
+
 public class Knapsack {
     static int[] values = new int[] {894, 260, 392, 281, 27};
     static int[] weights = new int[] {15, 6, 4, 2, 21};
-
+    static int[][] memo = new int[values.length][weights.length];
 
     public static Integer knapsack(int i, int W) {
         //if(lists.get(n).get(C) != null) {
         //    return lists.get(n).get(C);
-
-        if(i < 0 || W == 0) {
-            return 0;
+        int result = 0;
+       // if(memo[i][W] != 0) {
+       //     return memo[i][W];
+       // }
+        if(i == 0 || W == 0) {
+            result = 0;
         } else if (weights[i] > W) {
-            return knapsack(i-1, W);
+            result = knapsack(i-1, W);
         } else {
-            return Math.max(knapsack(i-1, W), knapsack(i-1, W - weights[i]) + values[i]);
+            result = Math.max(knapsack(i-1, W), knapsack(i-1, W - weights[i]) + values[i]);
         }
-
+        memo[i][W] = result;
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(knapsack(values.length-1, 30));
+        System.out.println(knapsack(values.length-1, 10));
     }
 }
