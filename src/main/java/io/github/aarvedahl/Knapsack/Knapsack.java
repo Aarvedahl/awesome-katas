93da77bf352a092eac29bb4945b6acaed1cb7e3f
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-06-13 15:36:30 +0200

    Continue with knapsack

--- src/main/java/io/github/aarvedahl/Knapsack/Knapsack.java
+++ src/main/java/io/github/aarvedahl/Knapsack/Knapsack.java
@@ -1,36 +1,41 @@
 package io.github.aarvedahl.Knapsack;
 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Knapsack {
     static int[] values = new int[]{894, 260, 392, 281, 27};
     static int[] weights = new int[]{15, 6, 4, 2, 21};
-    static Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
 
-    public static Integer knapsack(int i, int W) {
+
+    public static Integer knapsack(int i, double W, List<Integer> values, List<Integer> weights) {
         int result = 0;
-        if (map.containsKey(i)) {
-            System.out.println("Contains here");
-            return map.get(i).get(W);
-        }
         if (i == 0 || W == 0) {
             result = 0;
-        } else if (weights[i] > W) {
-            result = knapsack(i - 1, W);
+        } else if (weights.get(i) > W) {
+            result = knapsack(i - 1, W, values, weights);
         } else {
-            result = Math.max(knapsack(i - 1, W), knapsack(i - 1, W - weights[i]) + values[i]);
+            result = Math.max(knapsack(i - 1, W, values, weights), knapsack(i - 1, W - weights.get(i), values, weights) + values.get(i));
         }
-        
-        map.put(i, new HashMap<>());
-        map.get(i).put(W, result);
+
+
         return result;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(knapsack(values.length - 1, 10));
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        while(scanner.hasNext()) {
+            List<Integer> values = new ArrayList<>();
+            List<Integer> weights = new ArrayList<>();
+            double weight = scanner.nextDouble();
+            int objects = scanner.nextInt();
+            for(int i=0; i<objects; i++) {
+                values.add(scanner.nextInt());
+                weights.add(scanner.nextInt());
+            }
+            System.out.println(knapsack(values.size() -1, weight, values, weights));
+        }
     }
 }
 
