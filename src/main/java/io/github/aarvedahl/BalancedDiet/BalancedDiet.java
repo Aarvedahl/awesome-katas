cdbccb1ff37dedff49f819a2029241f77cccf2cb
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-06-23 19:46:36 +0200

    balanced diet

--- src/main/java/io/github/aarvedahl/BalancedDiet/BalancedDiet.java
+++ src/main/java/io/github/aarvedahl/BalancedDiet/BalancedDiet.java
@@ -4,38 +4,45 @@ import java.util.*;
 
 public class BalancedDiet {
 
+    static int[][] knapList;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<Integer> numList = new ArrayList<>();
-        while(scan.hasNextInt()) {
-            int times = scan.nextInt();
-            if(times == 0) {
+        while(true) {
+            int number = scan.nextInt();
+            if(number == 0) {
                 break;
             }
 
-            for(int i=0; i<times; i++) {
-                numList.add(scan.nextInt());
+            int[] cals = new int[number];
+            int sum = 0;
+
+            for(int i=0; i<cals.length; i++) {
+                cals[i] = scan.nextInt();
+                sum+=cals[i];
             }
 
-            int meal1 = 0, meal2 = 0;
+            knapList = new int[number+1][sum/2 +1];
 
-            for(int i=0; i<numList.size(); i++) {
+            int small = knapsack(cals, number, sum/2);
+            int big = sum - small;
 
+            System.out.println(big + " " + small);
+        }
+        scan.close();
+    }
+
+    public static int knapsack(int[] nums, int n, int W) {
+        for(int i=0; i<knapList.length; i++) {
+            for(int j=0; j<knapList[0].length; j++) {
+                if(i == 0 || j == 0) {
+                    knapList[i][j] = 0;
+                } else if(nums[i -1] > j) {
+                    knapList[i][j] = knapList[i-1][j];
+                } else {
+                    knapList[i][j] = Math.max(knapList[i-1][j], nums[i-1] + knapList[i-1][j-nums[i-1]]);
+                }
             }
-
-            System.out.println(meal1 + " " + meal2);
         }
-    }
-
-    public static void findBestNumber(List <Integer> integerList) {
-        for(int i=0; i<integerList.size(); i++) {
-
-        }
-    }
-    public static List<Integer> takeOneFromList(List<Integer> integerList, int sum) {
-        int firstInteger = integerList.get(0);
-        sum += firstInteger;
-        integerList.remove(firstInteger);
-        return integerList;
+        return knapList[n][W];
     }
 }
