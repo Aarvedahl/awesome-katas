73401d495b54587be80ae42d8fd5055c929af228
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-06-24 18:46:13 +0200

    More streams

--- src/main/java/io/github/aarvedahl/Streams.java
+++ src/main/java/io/github/aarvedahl/Streams.java
@@ -2,6 +2,7 @@ package io.github.aarvedahl;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
@@ -13,5 +14,35 @@ public class Streams {
         Stream<Integer> stream = numbers.stream()
                 .map(e -> e *2);
         stream.forEach(System.out::println);
+
+        int n = 101;
+        int k = 68;
+
+        System.out.println(compute(n, k));
+    }
+
+    public static double compute(int n, int k) {
+        int index = n;
+        int count = 0;
+
+        double result = 0;
+
+        while(count < k) {
+            if(isPrime(index)) {
+                result += Math.sqrt(index);
+                count++;
+            }
+            index++;
+        }
+
+        return Stream.iterate(n, e-> e +1)
+                .filter(Streams::isPrime)
+                .mapToDouble(Math::sqrt)
+                .limit(k)
+                .sum();
+    }
+
+    public static boolean isPrime(int n) {
+        return n > 1 && IntStream.range(2, n).noneMatch(i -> n%i == 0);
     }
 }
